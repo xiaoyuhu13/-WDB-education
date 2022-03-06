46a49cf19d952efa2c6785708ad7e9f247400d9e
xiaoyuhu13 <66574643+xiaoyuhu13@users.noreply.github.com>
2022-03-05 19:19:34 -0800

    assign 3

---
+++ assign3.js
@@ -0,0 +1,46 @@
+
+
+function max1(iterable, key) {
+    var maximum = 0;
+    for (var item of iterable) {
+      var thing = key(item);
+      if (thing >= maximum) {
+        maximum = item;
+      }
+    }
+    return maximum;
+  };
+
+var array = [1, 2, 3, 4, 5];
+function test(num) {
+ return num
+};
+
+console.log(max1(array, test));
+
+const reverser = function (item) {-item};
+
+function changer(object, key, value) {
+  var num = value;
+  if (Array.isArray(prev)) {
+    Array.append(num);
+  } else if (typeof prev == 'object' && prev != null) {
+    key = value;
+  } else {
+    object.append = [key, value];
+  }
+  return null;
+}
+
+async function promise_me(truthy) {
+  const a = new Promise((resolve, reject) => {
+      console.log(_____)
+      ______(______)
+  })
+  console.log(2)
+  console.log(3)
+  console.log(4)
+  const five = ______ ______
+  console.log(five)
+  return five
+}
